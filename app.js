0778201941c1254b15a270d12dff638480a92d59
James Brown <cdmdb@hotmail.com>
2019-06-11 18:34:25 +0200

    fixes

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ function start (author, permlink, blockNum) {
 	return new Promise(async (resolve, reject) => {
 		let block = {}
 		if (first_run) {
-			console.log('first run')
+			// console.log('first run')
 			let res      = await client.database.call('get_content', [author, permlink])
 			post_created = res.created
 			post_created = new Date(Date.parse(post_created) - n)
@@ -26,18 +26,18 @@ function start (author, permlink, blockNum) {
 		} else {
 			block = await client.database.getBlockHeader(blockNum)
 		}
-		console.log('\n ** blockNum = ' + blockNum + ' **')
+		// console.log('\n ** blockNum = ' + blockNum + ' **')
 		let block_time = new Date(Date.parse(block.timestamp) - n)
 		let timediff = (block_time - post_created) / 1000
-		console.log('timediff = ' + timediff + ' sec')
+		// console.log('timediff = ' + timediff + ' sec')
 		if (timediff > 3) {
 			let block_delta = timediff / sec_per_block
 			console.log('block_delta = ' + block_delta)
-			start(author, permlink, blockNum - block_delta)
+			return start(author, permlink, blockNum - block_delta).then((res) => { return resolve(res)})
 		} else if (timediff < 0) {
 			let block_delta = timediff / sec_per_block
 			console.log('block_delta = ' + block_delta)
-			start(author, permlink, blockNum - block_delta)
+			return start(author, permlink, blockNum - block_delta).then((res) => { return resolve(res)})
 		} else {
 			console.log('bingo, origin TRX has been found')
 			let block = await client.database.getBlock(blockNum + 1)
@@ -45,22 +45,22 @@ function start (author, permlink, blockNum) {
 			trxs.forEach((trx) => {
 				trx.operations.forEach((op) => {
 					if (op[0] == 'comment') {
-						console.log(op[1].permlink)
+						// console.log(op[1].permlink)
 						if (op[1].permlink == permlink) {
-							console.log('bingo, permlink has been found')
-							console.log(op[1].permlink)
+							// console.log('bingo, permlink has been found')
+							// console.log(op[1].permlink)
 							trx.operations.forEach((op) => {
 								if (op[0] == 'custom_json' && op[1].id == 'likwid-beneficiary') {
 									let json = JSON.parse(op[1].json)
-									console.log(json)
+									// console.log(json)
 									return resolve(json)
 								}
 							})
 						}
 					}
 				})
-				return resolve()
 			})
+			return resolve()
 		}
 	})
 }

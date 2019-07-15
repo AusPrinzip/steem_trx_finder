f1a42bce931cd9f19021203342c6576d672147ba
James Brown <cdmdb@hotmail.com>
2019-07-15 19:20:33 +0200

    added findVoteTrx method

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const dsteem         = require('dsteem')
-const client         = new dsteem.Client('https://api.steemit.com')
+// const dsteem         = require('dsteem')
+// const client         = new dsteem.Client('https://api.steemit.com')
 const MIN            = 60 * 1000
 const SEC            = 1000
 const  sec_per_block = 3
@@ -9,7 +9,7 @@ var n                = d.getTimezoneOffset() * MIN
 
 var post_created     = ''
 
-function start (author, permlink, blockNum) {
+function findCommentTrx (client, author, permlink, blockNum) {
 	return new Promise(async (resolve, reject) => {
 		let block     = {}
 		if (!blockNum) {
@@ -37,7 +37,7 @@ function start (author, permlink, blockNum) {
 			console.log('block_delta = ' + block_delta)
 			return start(author, permlink, blockNum - block_delta).then((res) => { return resolve(res)})
 		} else {
-			console.log('bingo, origin BLOCK has been found')
+			console.log('origin BLOCK has been found')
 			let block = await client.database.getBlock(blockNum + 1)
 			let trxs = block.transactions
 			trxs.forEach((trx) => {
@@ -62,7 +62,6 @@ function start (author, permlink, blockNum) {
 									return resolve(beneficiaries)
 								}
 							})
-							return resolve(trx)
 						}
 					}
 				})
@@ -72,7 +71,36 @@ function start (author, permlink, blockNum) {
 	})
 }
 
+function findVoteTrx (trans, _client) {
+	return new Promise(async (resolve, reject) => {
+		// console.log(trans.op[1])
+		let blockNum = trans.block
+		let block
+		try { 
+			block = await _client.database.getBlock(blockNum)
+		} catch(e) {
+			return reject(e)
+		}
+		// console.log('origin BLOCK has been found')
+		// let block = await client.database.getBlock(blockNum + 1)
+		let trxs = block.transactions
+		trxs.forEach((trx) => {
+			trx.operations.forEach((op) => {
+				if (op[0] == 'vote') {
+					// console.log(op[1])
+					if ( JSON.stringify(op[1]) === JSON.stringify(trans.op[1]) ) {
+						// console.log('bingo, TRX has been found')
+						return resolve(trx)
+					}
+				}
+			})
+		})
+		return reject()
+	})
+}
+
 module.exports = {
-	start: start
+	findCommentTrx: findCommentTrx,
+	findVoteTrx: findVoteTrx
 }
 

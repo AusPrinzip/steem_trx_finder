d381e2e01e47a87e3b7939e71aae17b97b3fad33
James Brown <cdmdb@hotmail.com>
2019-08-20 13:23:21 +0200

    code cleanup

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const dsteem         = require('dsteem')
-// const client         = new dsteem.Client('https://api.steemit.com')
 const MIN            = 60 * 1000
 const SEC            = 1000
 const  sec_per_block = 3
@@ -15,7 +14,6 @@ function findCommentTrx (client, author, permlink, blockNum, last_block_delta) {
 	return new Promise(async (resolve, reject) => {
 		let block     = {}
 		if (!blockNum) {
-			// console.log('first run')
 			let res      = await client.database.call('get_content', [author, permlink])
 			console.log('')
 			post_created = res.created
@@ -30,13 +28,8 @@ function findCommentTrx (client, author, permlink, blockNum, last_block_delta) {
 		} else {
 			block = await client.database.getBlockHeader(blockNum)
 		}
-		// console.log('\n ** blockNum = ' + blockNum + ' **')
-		// console.log(block)
 		let block_time = new Date(Date.parse(block.timestamp) - n)
 		let timediff = (block_time - post_created) / 1000
-		// console.log('block_time ' + block_time)
-		// console.log('post_created ' + post_created)
-		// console.log('timediff = ' + timediff + ' sec')
 		if (timediff > 3) {
 			let block_delta = timediff / sec_per_block
 			console.log('block_delta = ' + block_delta)
@@ -58,15 +51,11 @@ function findCommentTrx (client, author, permlink, blockNum, last_block_delta) {
 			trxs.forEach((trx) => {
 				trx.operations.forEach((op) => {
 					if (op[0] == 'comment') {
-						// console.log(op[1].permlink)
 						if (op[1].permlink == permlink) {
 							console.log('bingo, TRX has been found')
-							// console.log('bingo, permlink has been found')
-							// console.log(op[1].permlink)
 							trx.operations.forEach((op) => {
 								if (op[0] == 'custom_json' && op[1].id == 'likwid-beneficiary') {
 									let json = JSON.parse(op[1].json)
-									// console.log(json)
 									let beneficiaries = []
 									try {
 										beneficiaries = json.beneficiaries
@@ -97,15 +86,11 @@ function findVoteTrx (trans, _client) {
 		} catch(e) {
 			return reject(e)
 		}
-		// console.log('origin BLOCK has been found')
-		// let block = await client.database.getBlock(blockNum + 1)
 		let trxs = block.transactions
 		trxs.forEach((trx) => {
 			trx.operations.forEach((op) => {
 				if (op[0] == 'vote') {
-					// console.log(op[1])
 					if ( JSON.stringify(op[1]) === JSON.stringify(trans.op[1]) ) {
-						// console.log('bingo, TRX has been found')
 						return resolve(trx)
 					}
 				}

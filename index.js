f0afe38c7786dd3e1fcb77bbc3f524d1cc0c7cd5
James Brown <cdmdb@hotmail.com>
2019-12-02 21:30:41 +0100

    fixed promise antipattern

--- index.js
+++ index.js
@@ -10,22 +10,24 @@ var n                = d.getTimezoneOffset() * MIN
 var post_created     = ''
 
 function findCommentTrx (client, author, permlink, blockNum, last_block_delta) {
-	return new Promise(async (resolve, reject) => {
+	return new Promise((resolve, reject) => {
 		let block     = {}
 		if (!blockNum) {
-			let res      = await client.database.call('get_content', [author, permlink])
-			console.log('')
-			post_created = res.created
-			post_created = new Date(Date.parse(post_created) - n)
-			let post_age = new Date() - new Date(post_created)
+			client.database.call('get_content', [author, permlink])
+			.then(async (res) => {
+				post_created = res.created
+				post_created = new Date(Date.parse(post_created) - n)
+				let post_age = new Date() - new Date(post_created)
 
-			if (post_age > 3 * YEAR) return reject('post date error: older than 3 years')
+				if (post_age > 3 * YEAR) return reject('post date error: older than 3 years')
 
-			block        = await client.blockchain.getCurrentBlockHeader()
-			blockNum     = await client.blockchain.getCurrentBlockNum()
-			first_run    = false
+				block        = await client.blockchain.getCurrentBlockHeader()
+				blockNum     = await client.blockchain.getCurrentBlockNum()
+				first_run    = false
+			})
 		} else {
-			block = await client.database.getBlockHeader(blockNum)
+			client.database.getBlockHeader(blockNum)
+			.then((res) => block = res)
 		}
 		let block_time = new Date(Date.parse(block.timestamp) - n)
 		let timediff = (block_time - post_created) / 1000
@@ -45,28 +47,31 @@ function findCommentTrx (client, author, permlink, blockNum, last_block_delta) {
 			return findCommentTrx(client, author, permlink, blockNum - block_delta, block_delta).then((res) => { return resolve(res)})
 		} else {
 			console.log('origin BLOCK has been found')
-			let block = await client.database.getBlock(blockNum + 1)
-			let trxs = block.transactions
-			trxs.forEach((trx) => {
-				trx.operations.forEach((op) => {
-					if (op[0] == 'comment') {
-						if (op[1].permlink == permlink) {
-							console.log('bingo, TRX has been found')
-							trx.operations.forEach((op) => {
-								if (op[0] == 'custom_json' && op[1].id == 'likwid-beneficiary') {
-									let json = JSON.parse(op[1].json)
-									let beneficiaries = []
-									try {
-										beneficiaries = json.beneficiaries
-									} catch(e) {
-										console.log(e)
-										return reject('custom_json detected but missing beneficiaries array')
+			client.database.getBlock(blockNum + 1)
+			.then((res) => {
+				block = res
+				let trxs = block.transactions
+				trxs.forEach((trx) => {
+					trx.operations.forEach((op) => {
+						if (op[0] == 'comment') {
+							if (op[1].permlink == permlink) {
+								console.log('bingo, TRX has been found')
+								trx.operations.forEach((op) => {
+									if (op[0] == 'custom_json' && op[1].id == 'likwid-beneficiary') {
+										let json = JSON.parse(op[1].json)
+										let beneficiaries = []
+										try {
+											beneficiaries = json.beneficiaries
+										} catch(e) {
+											console.log(e)
+											return reject('custom_json detected but missing beneficiaries array')
+										}
+										return resolve(beneficiaries)
 									}
-									return resolve(beneficiaries)
-								}
-							})
+								})
+							}
 						}
-					}
+					})
 				})
 			})
 			console.log('trx could not be found')
@@ -76,14 +81,10 @@ function findCommentTrx (client, author, permlink, blockNum, last_block_delta) {
 }
 
 function findVoteTrx (client, pseudo_trx) {
-	return new Promise(async (resolve, reject) => {
+	client.database.getBlock(blockNum)
+	.then((res) => {
 		let blockNum = pseudo_trx[1].block
-		let block
-		try { 
-			block = await client.database.getBlock(blockNum)
-		} catch(e) {
-			return reject(e)
-		}
+		block = res
 		let trxs = block.transactions
 		trxs.forEach((trx) => {
 			trx.operations.forEach((op) => {

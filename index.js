fe8b41c412c9706afa2f95d1d62964c7a99aac04
James Brown <cdmdb@hotmail.com>
2019-12-02 23:04:10 +0100

    reworked async flow of promises simplified code by created a new function

--- index.js
+++ index.js
@@ -9,75 +9,75 @@ var n                = d.getTimezoneOffset() * MIN
 
 var post_created     = ''
 
-function findCommentTrx (client, author, permlink, blockNum, last_block_delta) {
+function findBlockNumber (author, permlink) {
 	return new Promise((resolve, reject) => {
-		let block     = {}
-		if (!blockNum) {
-			client.database.call('get_content', [author, permlink])
-			.then(async (res) => {
-				post_created = res.created
-				post_created = new Date(Date.parse(post_created) - n)
-				let post_age = new Date() - new Date(post_created)
+		client.database.call('get_content', [author, permlink])
+		.then((res) => {
+			post_created = res.created
+			post_created = new Date(Date.parse(post_created) - n)
+			let post_age = new Date() - new Date(post_created)
 
-				if (post_age > 3 * YEAR) return reject('post date error: older than 3 years')
+			if (post_age > 3 * YEAR) return reject('post date error: older than 3 years')
 
-				block        = await client.blockchain.getCurrentBlockHeader()
-				blockNum     = await client.blockchain.getCurrentBlockNum()
-				first_run    = false
+			// block        = await client.blockchain.getCurrentBlockHeader()
+			client.blockchain.getCurrentBlockNum()
+			.then((blockNum) => {
+				return resolve(blockNum)
 			})
-		} else {
-			client.database.getBlockHeader(blockNum)
-			.then((res) => block = res)
-		}
-		let block_time = new Date(Date.parse(block.timestamp) - n)
-		let timediff = (block_time - post_created) / 1000
-		if (timediff > 3) {
-			let block_delta = timediff / sec_per_block
-			console.log('block_delta = ' + block_delta)
-			return findCommentTrx(client, author, permlink, blockNum - block_delta, block_delta).then((res) => { return resolve(res)})
-		} else if (timediff < 0) {
-			let block_delta = timediff / sec_per_block
-			console.log('block_delta = ' + block_delta)
-			if (block_delta == -last_block_delta) {
-				console.log(blockNum)
-				console.log(blockNum - block_delta)
-				console.log('** loop detected **')
-				block_delta++
-			}
-			return findCommentTrx(client, author, permlink, blockNum - block_delta, block_delta).then((res) => { return resolve(res)})
-		} else {
-			console.log('origin BLOCK has been found')
-			client.database.getBlock(blockNum + 1)
-			.then((res) => {
-				block = res
-				let trxs = block.transactions
-				trxs.forEach((trx) => {
-					trx.operations.forEach((op) => {
-						if (op[0] == 'comment') {
-							if (op[1].permlink == permlink) {
-								console.log('bingo, TRX has been found')
-								trx.operations.forEach((op) => {
-									if (op[0] == 'custom_json' && op[1].id == 'likwid-beneficiary') {
-										let json = JSON.parse(op[1].json)
-										let beneficiaries = []
-										try {
-											beneficiaries = json.beneficiaries
-										} catch(e) {
-											console.log(e)
-											return reject('custom_json detected but missing beneficiaries array')
-										}
-										return resolve(beneficiaries)
-									}
-								})
-							}
-						}
-					})
-				})
-			})
-			console.log('trx could not be found')
-			return resolve()
-		}
+		})
 	})
+}
+
+async function findCommentTrx (client, author, permlink, blockNum, last_block_delta) {
+	if (!blockNum) {
+		first_run    = false
+		blockNum = await findBlockNumber(author, permlink)
+	} 
+	let block = await client.database.getBlockHeader(blockNum)
+	let block_time = new Date(Date.parse(block.timestamp) - n)
+	let timediff = (block_time - post_created) / 1000
+	if (timediff > 3) {
+		let block_delta = timediff / sec_per_block
+		console.log('block_delta = ' + block_delta)
+		return findCommentTrx(client, author, permlink, blockNum - block_delta, block_delta).then((res) => { return res})
+	} else if (timediff < 0) {
+		let block_delta = timediff / sec_per_block
+		console.log('block_delta = ' + block_delta)
+		if (block_delta == -last_block_delta) {
+			console.log(blockNum)
+			console.log(blockNum - block_delta)
+			console.log('** loop detected **')
+			block_delta++
+		}
+		return findCommentTrx(client, author, permlink, blockNum - block_delta, block_delta).then((res) => { return res})
+	} else {
+		console.log('origin BLOCK has been found')
+		block = await client.database.getBlock(blockNum + 1)
+		let trxs = block.transactions
+		trxs.forEach((trx) => {
+			trx.operations.forEach((op) => {
+				if (op[0] == 'comment') {
+					if (op[1].permlink == permlink) {
+						console.log('bingo, TRX has been found')
+						trx.operations.forEach((op) => {
+							if (op[0] == 'custom_json' && op[1].id == 'likwid-beneficiary') {
+								let json = JSON.parse(op[1].json)
+								let beneficiaries = []
+								try {
+									beneficiaries = json.beneficiaries
+								} catch(e) {
+									console.log(e)
+									throw new Error('custom_json detected but missing beneficiaries array')
+								}
+								return beneficiaries
+							}
+						})
+					}
+				}
+			})
+		})
+		console.log('trx could not be found')
+	}
 }
 
 function findVoteTrx (client, pseudo_trx) {
@@ -98,6 +98,7 @@ function findVoteTrx (client, pseudo_trx) {
 		throw new Error()
 	})
 }
+
 
 module.exports = {
 	findCommentTrx: findCommentTrx,
